4e2c25c38bc6c73ff86e52f9a91e3510eec8c440
Antonio Musarra <antonio.musarra@gmail.com>
2012-03-04 12:49:04 +0100

    Initial import.

---
+++ sugarcrmce-64-v4-soap-api/src/test/java/it/shirus/labs/sugarcrm/client/soap/SimpleClientTest.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package it.shirus.labs.sugarcrm.client.soap;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+
+import javax.xml.namespace.QName;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sugarcrm.sugarcrm.Sugarsoap;
+import com.sugarcrm.sugarcrm.SugarsoapPortType;
+
+/**
+ * @author amusarra
+ * 
+ */
+public class SimpleClientTest {
+
+	private static final QName SERVICE_NAME = new QName(
+			"http://www.sugarcrm.com/sugarcrm", "sugarsoap");
+	private URL wsdlURL;
+	
+	private Properties p = new Properties();
+
+	@Before
+	public void setUp() throws FileNotFoundException, IOException {
+		p.load(getClass().getResourceAsStream("SimpleClientTest.properties"));
+	}
+
+	@Test
+	public void simpleSugarCRMProcess() {
+		try {
+			wsdlURL = new URL(p.getProperty("sugarcrm.WSDL"));
+		} catch (MalformedURLException e) {
+			fail("WSDL Url failed: " + e.getMessage());
+		}
+		
+		Sugarsoap ss = new Sugarsoap(wsdlURL, SERVICE_NAME);
+		SugarsoapPortType port = ss.getSugarsoapPort();
+
+		System.out.println("SugarCRM Server Info...");
+		assertEquals("Version","6.4.0", port.getServerInfo().getVersion());
+		assertEquals("Flavor","CE", port.getServerInfo().getFlavor());		
+	}
+
+}
