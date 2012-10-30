20bcb0e352360f293208a151eeac879fab118599
Antonio Musarra <antonio.musarra@gmail.com>
2012-10-30 23:48:18 +0100

    Add another test as: Get Entry List and Get Module List.

--- src/test/java/it/shirus/labs/sugarcrm/client/soap/SimpleClientTest.java
+++ src/test/java/it/shirus/labs/sugarcrm/client/soap/SimpleClientTest.java
@@ -12,15 +12,28 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 import java.util.Properties;
 
 import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import com.sugarcrm.sugarcrm.EntryList;
+import com.sugarcrm.sugarcrm.EntryValue;
+import com.sugarcrm.sugarcrm.GetEntryListResultVersion2;
+import com.sugarcrm.sugarcrm.LinkNameToFieldsArray;
+import com.sugarcrm.sugarcrm.LinkNamesToFieldsArray;
+import com.sugarcrm.sugarcrm.ModuleList;
+import com.sugarcrm.sugarcrm.ModuleListEntry;
+import com.sugarcrm.sugarcrm.NameValue;
 import com.sugarcrm.sugarcrm.NameValueList;
+import com.sugarcrm.sugarcrm.SelectFields;
 import com.sugarcrm.sugarcrm.Sugarsoap;
 import com.sugarcrm.sugarcrm.SugarsoapPortType;
 import com.sugarcrm.sugarcrm.UserAuth;
@@ -33,16 +46,17 @@ public class SimpleClientTest {
 
 	private static final QName SERVICE_NAME = new QName(
 			"http://www.sugarcrm.com/sugarcrm", "sugarsoap");
-	
+
 	private static final String APPLICATION_NAME = Class.class.getName();
 	private URL wsdlURL;
 	private String sessionID = null;
-	
+
 	private Properties p = new Properties();
 
 	@Before
 	public void setUp() throws FileNotFoundException, IOException {
-		p.load((new ClassPathResource("SimpleClientTest.properties")).getInputStream());
+		p.load((new ClassPathResource("SimpleClientTest.properties"))
+				.getInputStream());
 	}
 
 	@Test
@@ -52,24 +66,36 @@ public class SimpleClientTest {
 		} catch (MalformedURLException e) {
 			fail("WSDL Url failed: " + e.getMessage());
 		}
-		
+
 		Sugarsoap ss = new Sugarsoap(wsdlURL, SERVICE_NAME);
 		SugarsoapPortType port = ss.getSugarsoapPort();
 
-		System.out.println("SugarCRM Server Info...");
-		assertEquals("Version","6.4.1", port.getServerInfo().getVersion());
-		assertEquals("Flavor","CE", port.getServerInfo().getFlavor());
-		System.out.println("SugarCRM Server Info...[ OK ]");
-		
+		System.out
+				.println("#--------------------------------------------------------------#");
+		System.out.println("# Invoking Server Info...");
+		System.out
+				.println("#--------------------------------------------------------------#");
+		System.out.println("# Version: " + port.getServerInfo().getVersion());
+		System.out.println("# Flavor: " + port.getServerInfo().getFlavor());
+
+		assertEquals("Version", "6.5.0", port.getServerInfo().getVersion());
+		assertEquals("Flavor", "CE", port.getServerInfo().getFlavor());
+		System.out
+		.println("#--------------------------------------------------------------#");
+
 		/**
 		 * Try to login on SugarCRM
 		 * 
-		 * 1) Prepare a MD5 hash password 
-		 * 2) Prepare a User Auth object 
-		 * 3) Execute login
+		 * 1) Prepare a MD5 hash password 2) Prepare a User Auth object 3)
+		 * Execute login
 		 */
 		{
-			System.out.println("Invoking login...");
+			System.out.println();
+			System.out
+					.println("#--------------------------------------------------------------#");
+			System.out.println("# Invoking login...");
+			System.out
+					.println("#--------------------------------------------------------------#");
 
 			// 1. Prepare a MD5 hash password
 			MessageDigest messageDiget = MessageDigest.getInstance("MD5");
@@ -81,19 +107,127 @@ public class SimpleClientTest {
 			_login_userAuth.setPassword((new BigInteger(1, messageDiget
 					.digest())).toString(16));
 			java.lang.String _login_applicationName = APPLICATION_NAME;
-			com.sugarcrm.sugarcrm.NameValueList _login_nameValueList = new NameValueList();	
+			com.sugarcrm.sugarcrm.NameValueList _login_nameValueList = new NameValueList();
 
 			try {
 				// 3. Execute login
 				com.sugarcrm.sugarcrm.EntryValue _login__return = port.login(
 						_login_userAuth, _login_applicationName,
 						_login_nameValueList);
-				System.out.println("Login Successfully for " + p.getProperty("sugarcrm.username"));
-				System.out.println("Your session Id: " + _login__return.getId());
+				System.out.println("# Login Successfully for "
+						+ p.getProperty("sugarcrm.username"));
+				System.out
+						.println("# Your session Id: " + _login__return.getId());
 				sessionID = _login__return.getId();
+				System.out
+				.println("#--------------------------------------------------------------#");
 			} catch (Exception e) {
 				fail("Login failed. Message: " + e.getMessage());
 			}
 		}
+
+		/**
+		 * Try get entry list operation
+		 */
+		{
+			System.out.println();
+			System.out
+					.println("#--------------------------------------------------------------#");
+			System.out.println("# Invoking Get Account Entry List...");
+
+			try {
+				SelectFields selectFields = new SelectFields();
+				LinkNamesToFieldsArray linkNamesToFieldsArray = new LinkNamesToFieldsArray();
+
+				Document doc = DocumentBuilderFactory.newInstance()
+						.newDocumentBuilder().newDocument();
+
+				Element fieldId = doc.createElement("item");
+				fieldId.setTextContent("id");
+
+				Element fieldName = doc.createElement("item");
+				fieldName.setTextContent("name");
+
+				Element fieldBillingCity = doc.createElement("item");
+				fieldBillingCity.setTextContent("billing_address_city");
+
+				selectFields.setArrayType("xsd:string[]");
+				selectFields.getAny().add((Element) fieldId);
+				selectFields.getAny().add((Element) fieldName);
+				selectFields.getAny().add((Element) fieldBillingCity);
+
+				linkNamesToFieldsArray
+						.setArrayType("ns1:link_name_to_fields_array[]");
+
+				GetEntryListResultVersion2 resultList = port.getEntryList(
+						sessionID, "Accounts", "", "", 5, selectFields,
+						linkNamesToFieldsArray, 3, 0, false);
+
+				System.out.println("# Total Count: "
+						+ resultList.getTotalCount());
+				System.out.println("# Result Count: "
+						+ resultList.getResultCount());
+				System.out
+						.println("#--------------------------------------------------------------#");
+
+				EntryList entryList = resultList.getEntryList();
+				List<EntryValue> entryValue = entryList.getItem();
+
+				for (EntryValue entry : entryValue) {
+					System.out
+							.println("#--------------------------------------------------------------#");
+					NameValueList nameValueList = entry.getNameValueList();
+					List<NameValue> nameValueItem = nameValueList.getItem();
+
+					System.out.println("# Account Entry ID: " + entry.getId());
+
+					for (NameValue nameValue : nameValueItem) {
+						System.out.println("# Account Field Name: "
+								+ nameValue.getName());
+						System.out.println("# Account Field Value: "
+								+ nameValue.getValue());
+					}
+
+					System.out
+							.println("#--------------------------------------------------------------#");
+
+				}
+			} catch (Exception e) {
+				fail("Get Entry failed: " + e.getMessage());
+			}
+		}
+
+		/**
+		 * Try get modules
+		 */
+		{
+			System.out.println();
+			System.out
+					.println("#--------------------------------------------------------------#");
+			System.out.println("# Invoking Get Module List...");
+			System.out
+					.println("#--------------------------------------------------------------#");
+
+			try {
+				ModuleList moduleList = port.getAvailableModules(sessionID, "");
+				List<ModuleListEntry> module = moduleList.getModules()
+						.getItem();
+
+				for (ModuleListEntry moduleListEntry : module) {
+					System.out
+					.println("#--------------------------------------------------------------#");
+					System.out.println("# Module Key:"
+							+ moduleListEntry.getModuleKey());
+					System.out.println("# Module Label:"
+							+ moduleListEntry.getModuleLabel());
+					System.out
+					.println("#--------------------------------------------------------------#");
+				}
+
+			} catch (Exception e) {
+				fail("Get Modules failed: " + e.getMessage());
+			}
+		}
+
 	}
 }
