d87176670ce7bffd8e06354cbfb590ca0256eb2f
Antonio Musarra <antonio.musarra@gmail.com>
2012-11-25 21:51:41 +0100

    Add new test getEntryList

--- src/test/java/it/shirus/labs/sugarcrm/client/soap/SimpleClientTest.java
+++ src/test/java/it/shirus/labs/sugarcrm/client/soap/SimpleClientTest.java
@@ -12,12 +12,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
 import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -27,8 +29,14 @@ import org.w3c.dom.Element;
 import com.sugarcrm.sugarcrm.EntryList;
 import com.sugarcrm.sugarcrm.EntryValue;
 import com.sugarcrm.sugarcrm.GetEntryListResultVersion2;
+import com.sugarcrm.sugarcrm.LinkArrayList;
+import com.sugarcrm.sugarcrm.LinkList;
+import com.sugarcrm.sugarcrm.LinkList2;
+import com.sugarcrm.sugarcrm.LinkLists;
 import com.sugarcrm.sugarcrm.LinkNameToFieldsArray;
+import com.sugarcrm.sugarcrm.LinkNameValue;
 import com.sugarcrm.sugarcrm.LinkNamesToFieldsArray;
+import com.sugarcrm.sugarcrm.LinkValue2;
 import com.sugarcrm.sugarcrm.ModuleList;
 import com.sugarcrm.sugarcrm.ModuleListEntry;
 import com.sugarcrm.sugarcrm.NameValue;
@@ -48,8 +56,10 @@ public class SimpleClientTest {
 			"http://www.sugarcrm.com/sugarcrm", "sugarsoap");
 
 	private static final String APPLICATION_NAME = Class.class.getName();
-	private URL wsdlURL;
-	private String sessionID = null;
+	private static URL wsdlURL;
+	private static String sessionID = null;
+	private static Sugarsoap ss = null;
+	private static SugarsoapPortType port;
 
 	private Properties p = new Properties();
 
@@ -57,19 +67,20 @@ public class SimpleClientTest {
 	public void setUp() throws FileNotFoundException, IOException {
 		p.load((new ClassPathResource("SimpleClientTest.properties"))
 				.getInputStream());
-	}
 
-	@Test
-	public void simpleSugarCRMProcess() throws NoSuchAlgorithmException {
 		try {
 			wsdlURL = new URL(p.getProperty("sugarcrm.WSDL"));
 		} catch (MalformedURLException e) {
 			fail("WSDL Url failed: " + e.getMessage());
 		}
 
-		Sugarsoap ss = new Sugarsoap(wsdlURL, SERVICE_NAME);
-		SugarsoapPortType port = ss.getSugarsoapPort();
+		ss = new Sugarsoap(wsdlURL, SERVICE_NAME);
+		port = ss.getSugarsoapPort();
 
+	}
+
+	@Test
+	public void getServerInfo() {
 		System.out
 				.println("#--------------------------------------------------------------#");
 		System.out.println("# Invoking Server Info...");
@@ -78,11 +89,15 @@ public class SimpleClientTest {
 		System.out.println("# Version: " + port.getServerInfo().getVersion());
 		System.out.println("# Flavor: " + port.getServerInfo().getFlavor());
 
-		assertEquals("Version", "6.5.0", port.getServerInfo().getVersion());
-		assertEquals("Flavor", "CE", port.getServerInfo().getFlavor());
+		assertEquals("Version mismatch", "6.5.0", port.getServerInfo()
+				.getVersion());
+		assertEquals("Edition mismatch", "CE", port.getServerInfo().getFlavor());
 		System.out
-		.println("#--------------------------------------------------------------#");
+				.println("#--------------------------------------------------------------#");
+	}
 
+	@Test
+	public void login() {
 		/**
 		 * Try to login on SugarCRM
 		 * 
@@ -98,7 +113,12 @@ public class SimpleClientTest {
 					.println("#--------------------------------------------------------------#");
 
 			// 1. Prepare a MD5 hash password
-			MessageDigest messageDiget = MessageDigest.getInstance("MD5");
+			MessageDigest messageDiget = null;
+			try {
+				messageDiget = MessageDigest.getInstance("MD5");
+			} catch (NoSuchAlgorithmException e1) {
+				fail(e1.getMessage());
+			}
 			messageDiget.update(p.getProperty("sugarcrm.password").getBytes());
 
 			// 2. Prepare a User Auth object
@@ -116,118 +136,179 @@ public class SimpleClientTest {
 						_login_nameValueList);
 				System.out.println("# Login Successfully for "
 						+ p.getProperty("sugarcrm.username"));
-				System.out
-						.println("# Your session Id: " + _login__return.getId());
+				System.out.println("# Your session Id: "
+						+ _login__return.getId());
 				sessionID = _login__return.getId();
+				assertTrue("Invalid len SessionId", sessionID.length() == 26);
 				System.out
-				.println("#--------------------------------------------------------------#");
+						.println("#--------------------------------------------------------------#");
 			} catch (Exception e) {
 				fail("Login failed. Message: " + e.getMessage());
 			}
 		}
+	}
 
-		/**
-		 * Try get entry list operation
-		 */
-		{
-			System.out.println();
-			System.out
-					.println("#--------------------------------------------------------------#");
-			System.out.println("# Invoking Get Account Entry List...");
+	@Test
+	public void getAvailableModules() {
+		System.out.println();
+		System.out
+				.println("#--------------------------------------------------------------#");
+		System.out.println("# Invoking Get Module List...");
+		System.out.println("# SessionId: " + sessionID);
+		System.out
+				.println("#--------------------------------------------------------------#");
 
-			try {
-				SelectFields selectFields = new SelectFields();
-				LinkNamesToFieldsArray linkNamesToFieldsArray = new LinkNamesToFieldsArray();
+		try {
+			ModuleList moduleList = port.getAvailableModules(sessionID, "");
+			List<ModuleListEntry> module = moduleList.getModules().getItem();
 
-				Document doc = DocumentBuilderFactory.newInstance()
-						.newDocumentBuilder().newDocument();
+			for (ModuleListEntry moduleListEntry : module) {
+				System.out
+						.println("#--------------------------------------------------------------#");
+				System.out.println("# Module Key:"
+						+ moduleListEntry.getModuleKey());
+				System.out.println("# Module Label:"
+						+ moduleListEntry.getModuleLabel());
+				System.out
+						.println("#--------------------------------------------------------------#");
+			}
 
-				Element fieldId = doc.createElement("item");
+		} catch (Exception e) {
+			fail("Get Modules failed: " + e.getMessage());
+		}
+	}
+
+	@Test
+	public void getEntryList() {
+		System.out.println();
+		System.out
+				.println("#--------------------------------------------------------------#");
+		System.out.println("# Invoking Get Account Entry List...");
+		System.out.println("# SessionId: " + sessionID);
+
+		try {
+			SelectFields entrySelectFields = new SelectFields();
+			{
+
+				Document docEntrySelectFields = DocumentBuilderFactory
+						.newInstance().newDocumentBuilder().newDocument();
+
+				Element fieldId = docEntrySelectFields.createElement("item");
 				fieldId.setTextContent("id");
 
-				Element fieldName = doc.createElement("item");
+				Element fieldName = docEntrySelectFields.createElement("item");
 				fieldName.setTextContent("name");
 
-				Element fieldBillingCity = doc.createElement("item");
+				Element fieldBillingCity = docEntrySelectFields
+						.createElement("item");
 				fieldBillingCity.setTextContent("billing_address_city");
 
-				selectFields.setArrayType("xsd:string[]");
-				selectFields.getAny().add((Element) fieldId);
-				selectFields.getAny().add((Element) fieldName);
-				selectFields.getAny().add((Element) fieldBillingCity);
+				entrySelectFields.setArrayType("ns1:select_fields_array[]");
+				entrySelectFields.getAny().add((Element) fieldId);
+				entrySelectFields.getAny().add((Element) fieldName);
+				entrySelectFields.getAny().add((Element) fieldBillingCity);
+			}
+
+			LinkNamesToFieldsArray linkNamesToFieldsArray = new LinkNamesToFieldsArray();
+			{
+				SelectFields linkSelectFields = new SelectFields();
+
+				Document docLinkSelectFields = DocumentBuilderFactory
+						.newInstance().newDocumentBuilder().newDocument();
+
+				Element fieldId = docLinkSelectFields.createElement("item");
+				fieldId.setTextContent("id");
+
+				Element fieldName = docLinkSelectFields.createElement("item");
+				fieldName.setTextContent("name");
+
+				Element fieldEmail = docLinkSelectFields
+						.createElement("item");
+				fieldEmail.setTextContent("email1");
+
+				linkSelectFields.setArrayType("xsd:string[]");
+				linkSelectFields.getAny().add((Element) fieldId);
+				linkSelectFields.getAny().add((Element) fieldName);
+				linkSelectFields.getAny().add((Element) fieldEmail);
+
+				LinkNameToFieldsArray linkNameToFieldsArray = new LinkNameToFieldsArray();
+				linkNameToFieldsArray.setName("contacts");
+				linkNameToFieldsArray.setValue(linkSelectFields);
 
 				linkNamesToFieldsArray
 						.setArrayType("ns1:link_name_to_fields_array[]");
+				linkNamesToFieldsArray.getAny().add(
+						(LinkNameToFieldsArray) linkNameToFieldsArray);
+			}
 
-				GetEntryListResultVersion2 resultList = port.getEntryList(
-						sessionID, "Accounts", "", "", 5, selectFields,
-						linkNamesToFieldsArray, 3, 0, false);
 
-				System.out.println("# Total Count: "
-						+ resultList.getTotalCount());
-				System.out.println("# Result Count: "
-						+ resultList.getResultCount());
+			GetEntryListResultVersion2 resultList = port.getEntryList(
+					sessionID, "Accounts", "", "", 5, entrySelectFields,
+					linkNamesToFieldsArray, 3, 0, false);
+
+			System.out.println("# Total Count: " + resultList.getTotalCount());
+			System.out
+					.println("# Result Count: " + resultList.getResultCount());
+			System.out
+					.println("#--------------------------------------------------------------#");
+
+			EntryList entryList = resultList.getEntryList();
+			LinkLists relationshipList = resultList.getRelationshipList();
+			
+			List<EntryValue> entryValue = entryList.getItem();
+
+			// Get Entry List
+			for (EntryValue entry : entryValue) {
+				System.out
+						.println("#--------------------------------------------------------------#");
+				NameValueList nameValueList = entry.getNameValueList();
+				List<NameValue> nameValueItem = nameValueList.getItem();
+
+				System.out.println("# Account Entry ID: " + entry.getId());
+
+				for (NameValue nameValue : nameValueItem) {
+					System.out.println("# Account Field Name: "
+							+ nameValue.getName());
+					System.out.println("# Account Field Value: "
+							+ nameValue.getValue());
+				}
+
 				System.out
 						.println("#--------------------------------------------------------------#");
 
-				EntryList entryList = resultList.getEntryList();
-				List<EntryValue> entryValue = entryList.getItem();
-
-				for (EntryValue entry : entryValue) {
-					System.out
-							.println("#--------------------------------------------------------------#");
-					NameValueList nameValueList = entry.getNameValueList();
-					List<NameValue> nameValueItem = nameValueList.getItem();
-
-					System.out.println("# Account Entry ID: " + entry.getId());
-
-					for (NameValue nameValue : nameValueItem) {
-						System.out.println("# Account Field Name: "
-								+ nameValue.getName());
-						System.out.println("# Account Field Value: "
-								+ nameValue.getValue());
+			}
+			
+			// Get Relationship list
+			System.out
+			.println("#--------------------------------------------------------------#");
+			System.out
+			.println("# Get related Contact for Account                              #");
+			System.out
+			.println("#--------------------------------------------------------------#");
+			List<LinkList2> relationships = relationshipList.getItem();
+			for (LinkList2 relationship : relationships) {
+				LinkList linkListItem = relationship.getLinkList();
+				List<LinkNameValue> nameValueItem = linkListItem.getItem();
+				for (LinkNameValue linkNameValue : nameValueItem) {
+					LinkArrayList records = linkNameValue.getRecords();
+					List<LinkValue2> nameValueItemRecord = records.getItem();
+					for (LinkValue2 linkValue2 : nameValueItemRecord) {
+						for (Iterator<NameValue> iterator = linkValue2.getLinkValue().getItem().iterator() ; iterator
+								.hasNext();) {
+							NameValue nameValueItemRecord1 = (NameValue) iterator
+									.next();
+							System.out.println("# Contact Field Name: "
+									+ nameValueItemRecord1.getName());
+							System.out.println("# Contact Field Value: "
+									+ nameValueItemRecord1.getValue());
+						}
 					}
-
-					System.out
-							.println("#--------------------------------------------------------------#");
-
 				}
-			} catch (Exception e) {
-				fail("Get Entry failed: " + e.getMessage());
 			}
-		}
-
-		/**
-		 * Try get modules
-		 */
-		{
-			System.out.println();
 			System.out
-					.println("#--------------------------------------------------------------#");
-			System.out.println("# Invoking Get Module List...");
-			System.out
-					.println("#--------------------------------------------------------------#");
-
-			try {
-				ModuleList moduleList = port.getAvailableModules(sessionID, "");
-				List<ModuleListEntry> module = moduleList.getModules()
-						.getItem();
-
-				for (ModuleListEntry moduleListEntry : module) {
-					System.out
-					.println("#--------------------------------------------------------------#");
-					System.out.println("# Module Key:"
-							+ moduleListEntry.getModuleKey());
-					System.out.println("# Module Label:"
-							+ moduleListEntry.getModuleLabel());
-					System.out
-					.println("#--------------------------------------------------------------#");
-				}
-
-			} catch (Exception e) {
-				fail("Get Modules failed: " + e.getMessage());
-			}
+			.println("#--------------------------------------------------------------#");
+		} catch (Exception e) {
+			fail("Get Entry failed: " + e.getMessage());
 		}
-
 	}
 }
